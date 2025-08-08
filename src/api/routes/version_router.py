dea5888cccb66629cf673d09a795a28f046197fb
utikpuhlik <utikpuhlik@win.com>
2025-08-08 13:08:31 +0200

    refactor: project

--- src/api/routes/version_router.py
+++ src/api/routes/version_router.py
@@ -1,12 +1,24 @@
+import importlib.metadata
+
 from fastapi import APIRouter
 
-from src.api.controllers.api_microservice_version import get_microservice_version
+from src.config import settings
+from src.utils.logging import logger
 
 router = APIRouter(prefix="/-", tags=["Utils"])
 
 
 @router.get("/version", response_model=dict, status_code=200)
-async def api_microservice_version() -> dict:
-    """Returns microservice version from pyproject.toml"""
-    version = await get_microservice_version()
-    return {"version": version}
+async def api_microservice_version():
+    """
+    Returns microservice version.\f (from pyproject.toml)
+    """
+    version_number = None
+
+    try:
+        version_number = importlib.metadata.version(settings.PROJECT_NAME)
+        logger.debug("Microservice version: %s", version_number)
+    except Exception as e:
+        logger.error(e)
+
+    return {"version": version_number}
