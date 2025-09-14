6808451a839e3385a9e78e42cfe5c8a1827f6b5c
Vadym Tatarinov <utikpuhlik@protonmail.ch>
2025-09-14 10:32:30 +0200

    feat(log): add auth for audit_log router

--- src/api/routes/audit_log_router.py
+++ src/api/routes/audit_log_router.py
@@ -2,11 +2,17 @@ from fastapi import APIRouter, status
 from fastapi.params import Depends
 from fastapi_pagination import Page
 
+from api.auth.clerk import require_role
 from api.di.db_helper import db_helper
 from schemas.audit_log_schema import AuditLogSchema
+from schemas.common.enums import Role
 from src.api.dao.audit_log_dao import AuditLogDAO
 
-router = APIRouter(tags=["Audit Log"], prefix="/audit-log")
+router = APIRouter(
+    tags=["Audit Log"],
+    prefix="/audit-log",
+    dependencies=[Depends(require_role(Role.EMPLOYEE))],
+)
 
 
 @router.get(
